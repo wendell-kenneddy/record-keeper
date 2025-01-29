422ce7b68730f3bc88d013d531fa5b8479d008fc
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-01-29 20:33:37 -0300

    feat(components): add artist-preview

---
+++ src/components/artist-preview/index.tsx
@@ -0,0 +1,26 @@
+import { Stack, Title } from "@mantine/core";
+import Link from "next/link";
+import linkStyles from "@/styles/link.module.css";
+
+interface ArtistPreviewProps {
+  id: number;
+  name: string;
+}
+
+export function ArtistPreview({ id, name }: ArtistPreviewProps) {
+  return (
+    <Stack
+      pl="xs"
+      pb="md"
+      pt="md"
+      align="flex-start"
+      style={{ borderLeft: "4px solid var(--mantine-color-teal-6)" }}
+    >
+      <Title order={3}>
+        <Link href={`/artists/${id}`} className={linkStyles.unstyled}>
+          {name}
+        </Link>
+      </Title>
+    </Stack>
+  );
+}
