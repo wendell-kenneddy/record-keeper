3b145d20e5716c96093e8827a2d2b80449e540b4
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-02-03 14:25:06 -0300

    refactor(components/header): leave only the logo displayed

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,14 @@
-import { Center, Flex, Group, Text } from "@mantine/core";
+import { Center, Flex } from "@mantine/core";
+import Image from "next/image";
 import Link from "next/link";
-import linkStyles from "@/styles/link.module.css";
 
 export function Header() {
   return (
     <Center w="full" h={64} bg="dark">
       <Flex w="100%" maw={1024} align="center" justify="space-between">
         <Link href="/" style={{ display: "flex", alignItems: "center" }}>
-          <Text>record.keeper</Text>
+          <Image src="/logo.svg" alt="Logo" width={106} height={19} priority />
         </Link>
-
-        <Group gap="xs">
-          <Link href="/artists" className={linkStyles.navLink}>
-            <Text>Artistas</Text>
-          </Link>
-        </Group>
       </Flex>
     </Center>
   );
